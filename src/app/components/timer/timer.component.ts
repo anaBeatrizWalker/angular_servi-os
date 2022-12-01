081d82164070cbfc873e69ff57c4478205fd0379
anaBeatrizWalker <95242519+anaBeatrizWalker@users.noreply.github.com>
2022-12-01 17:35:42 -0300

    Corrigindo código

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.css']
+  styleUrls: ['./timer.component.css'],
 })
 export class TimerComponent implements OnInit {
+  private timer: any;
+  private counter: number = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.timer = setInterval(() => {
+      this.counter++;
+    }, 1000);
   }
 
+  ngOnInit() {}
+
+  getCounter() {
+    return this.counter;
+  }
 }
